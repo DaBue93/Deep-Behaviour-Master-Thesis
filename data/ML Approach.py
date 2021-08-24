62bdeb59eee9ac0de04cd770e74f30f60ecbb3e8
DaJe93 <66160679+DaJe93@users.noreply.github.com>
2021-08-24 11:40:53 +0200

    Update ML Approach.py

--- data/ML Approach.py
+++ data/ML Approach.py
@@ -46,3 +46,28 @@ for i in rows.iterrows():
     else:
         behaviour.append(0)
 df["OF_anxiety"] = behaviour
+
+
+###EPM PREDICTION###
+X= df[['EPM_total_distance_cm', 'EPM_velolocity_cm/s', 'EPM_mobility_s',
+       'EPM_immobility_s', 'EPM_open_arm_freq_counts',
+       'EPM_open_arm_duration_s', 'EPM_closed_arm_freq_counts',
+       'EPM_closed_arm_duration_s', 'EPM_center_freq_counts',
+       'EPM__center_duration_s', 'EPM_vertikal_activity_counts']]
+
+
+from sklearn.cluster import KMeans
+model = KMeans(n_clusters=2)
+model.fit(X)
+print(model.labels_)
+
+###TRAIN TEST SPLIT###
+from sklearn.model_selection import train_test_split
+train_test_split
+
+###PREDICT###
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+model.fit(X_train,y_train)
+
+print(model.score(X_test, y_test))
